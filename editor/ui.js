5d1e66ba461e68dcf4160356c3fe13b0a3751b0b
asterisk <6507055@gmail.com>
2024-01-22 02:25:11 +0900

    editor 구현중

--- editor/ui.js
+++ editor/ui.js
@@ -1,30 +1,40 @@
 let app;
 let files = [
-    { name: 'files_1.txt' },
-    { name: 'index.css' },
-    { name: 'index', isFolder: true, children: [{ name: 'index.html' }] },
-    { name: 'index.html' }
+    { id: Math.random(), name: 'files_1.txt' },
+    { id: Math.random(), name: 'index.css' },
+    { id: Math.random(), name: 'index', isFolder: true, children: [{ name: 'index.html' }] },
+    { id: Math.random(), name: 'index.html' }
 ]
 Vue.component('file', {
     props: {
+        'id': {},
         'name': { default: '[no_named_file]' },
         'isFolder': { default: false },
         'children': { default: () => [] },
+        'state': { type: Number },
         'onTab': {}
     },
     template: `
     <div class="aside_folder" v-if="isFolder">
-        <div class="aside_line mdi mdi-folder" onclick="this.parentElement.classList.toggle('open')" tabindex="0"> {{ name }}</div>
+        <div class="aside_line" onclick="this.parentElement.classList.toggle('open')" tabindex="0"> {{ name }}</div>
         <file v-for="child in children" v-bind="{...child,onTab}"></file>
     </div>
-    <div class="aside_line" v-else v-on:click="onTab.newSubTab({name, isFolder, onTab})" tabindex="0">{{ name }}</div>
+    <div class="aside_line" v-else 
+        v-on:click="onTab.addSubTab({id, name, isFolder, onTab}, true)"
+        v-on:dblclick="onTab.addSubTab({id})"
+        :type="name.split('.')[1]||'file'"
+        tabindex="0"> {{ name }}</div>
     `
 })
+const TabState = {
+    temp: 0,
+    open: 1
+}
 
 class Tab {
-    constructor(app) {
+    constructor(_app) {
         this._uid = Math.random();
-        this._app = app;
+        this._app = _app;
         this._icon = 'mdi-radiobox-blank';
         this._nam = 'empty tab';
         this._subTabs = [];
@@ -32,18 +42,36 @@ class Tab {
         this._onSubTab = new SubTab();
     }
 
-    newSubTab = ({
-        name = 'file1.txt'
-    }) => {
-        let subTab = new SubTab(this, { name });
-        this._subTabs.push(subTab);
+    addSubTab = ({
+        name = 'file1.txt',
+        id
+    }, temp = false) => {
+        let subTab = null, temp_index = this._subTabs.findIndex(subTab => subTab._state == TabState.temp);
+        let state = temp ? TabState.temp : TabState.open;
+
+        if (temp_index < 0) {
+            if (temp_index = this._subTabs.findIndex(subTab => subTab._uid == id) > -1) return;
+            this._app.onTab._onSubTab = subTab = create();
+            this._subTabs.push(subTab);
+        } else if (this._subTabs[temp_index]._uid === id) {
+            this._app.onTab._onSubTab = subTab = this._subTabs[temp_index];
+            subTab.set('state', state);
+        } else {
+            this._app.$set(this._app.onTab._subTabs, temp_index, create());
+            this._app.onTab._onSubTab = subTab = this._subTabs[temp_index];
+        }
+
+        function create() {
+            return new SubTab(this, { uid: id, name, state });
+        }
+
         return subTab;
     }
 }
 
 class GameTab extends Tab {
-    constructor(app) {
-        super(app);
+    constructor(_app) {
+        super(_app);
         this._nam = 'game develop';
         this._icon = 'mdi-gamepad-square';
     }
@@ -55,22 +83,36 @@ class SubTab {
         this._tab = tab;
         for (let propName in props) this['_' + propName] = props[propName];
     }
+
+    set = (p, v) => this['_' + p] = v;
 }
 
 window.onload = () => {
     app = new Vue({
         el: '.bodyContainer',
+        updated: () => {
+            repaintScrollbarVisible();
+        },
         data: {
             files,
             tabs: [],
             onTab: new Tab(),
         },
         methods: {
+            addTab: function () {
+                let tab = new Tab(this);
+                this.tabs.push(tab);
+            },
+            changeTab: function (tab) {
+                this.onTab = tab;
+            }
         }
     });
-    app.tabs.push(new Tab(app));
-    app.tabs.push(new Tab(app));
-    app.tabs.push(new Tab(app));
+    app.addTab();
+    app.addTab();
+    app.addTab();
+    app.onTab._app = app;
+
     let aside = document.querySelector('section');
     let resizer = document.querySelector('.resizer');
 
@@ -87,24 +129,77 @@ window.onload = () => {
 }
 
 window.addEventListener('resize', function () {
-    console.log(1);
+    repaintScrollbarVisible();
+})
 
-    for (let scrollbar of document.querySelectorAll('.h-scrollbar')) {
-        console.log(scrollbar);
-        let target = document.querySelector(scrollbar.getAttribute('target'));
+function repaintScrollbar(scrollbar) {
+    let target = document.querySelector(scrollbar.getAttribute('target'));
 
-        let w = target.getBoundingClientRect().width;
-        let scroll_w = target.scrollWidth;
-        let scroll_l = target.scrollLeft;
-        let offset = w / scroll_w;
-        let scroll_size_w = w * offset;
-        
-        console.log(offset, scroll_w, w);
-        scrollbar.style.width = Math.floor(scroll_size_w) + 'px';
-        scrollbar.style.left = Math.floor((w - scroll_size_w) * offset * (scroll_l - scroll_w) / scroll_l) + 'px';
+    let w = target.getBoundingClientRect().width;
+    let scroll_w = target.scrollWidth;
+    let ratio = w / scroll_w;
+    if (scroll_w - w < 2) return scrollbar.classList.add('unused');
+    scrollbar.classList.remove('unused');
 
-        scrollbar.onmousedown = ()=>{
-            
+    let offset = target.scrollLeft / scroll_w;
+
+    let min = 0, max = (scroll_w - w) * ratio;
+
+    scrollbar.style.width = Math.floor(w * ratio) + 'px';
+    scrollbar.style.left = Math.floor(w * offset) + 'px';
+
+    scrollbar.onmousedown = e_down => {
+        e_down.preventDefault();
+        let pos = (parseInt(scrollbar.getAttribute('pos')) || 0);
+        let pos_down = e_down.screenX;
+        window.onmouseup = () => {
+            window.onmouseup = window.onmousemove = undefined;
+        }
+        window.onmousemove = e_move => {
+            let pos_move = e_move.screenX;
+            let pos_final = between(min, max, (pos_move - pos_down + pos));
+            target.scrollLeft = pos_final / ratio;
+            scrollbar.style.left = pos_final + 'px';
         }
     }
-})
+}
+
+function repaintScrollbarVisible() {
+    for (let scrollbar of document.querySelectorAll('.h-scrollbar')) repaintScrollbar(scrollbar);
+}
+
+function between(min, max, value) {
+    return Math.max(Math.min(max, value), min);
+}
+
+
+const editor = {
+    focus: undefined,
+    on: {
+        keydown: ({ keyCode, key }) => {
+            console.log(keyCode, key);
+            if (keyCode > 64 && keyCode < 91) {
+                this.focus.append(document.createTextNode(key));
+            } else {
+                switch (keyCode) {
+                    case 8:
+                        this.focus.innerHTML = '';
+                        break;
+                    case 13:
+                        let line = document.createElement('div');
+                        line.classList.add('line');
+                        line.onclick = function(){
+                            console.log('ok');
+                            this.focus = line;
+                        }
+                        document.querySelector('.subTab__contents').append(line);
+                        break;
+                }
+            }
+
+        },
+        keyup: ({ keyCode }) => {
+
+        }
+    }
+}
