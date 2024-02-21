8a4c1bccbf327bb893a2b7f71f095212c12b33c7
asterisk <6507055@gmail.com>
2024-02-22 04:50:40 +0900

    input form

--- wiki/form.js
+++ wiki/form.js
@@ -1,11 +1,41 @@
 let main__contents;
 function drop(e) {
     e.preventDefault();
-
+    let files = e.dataTransfer.files || e.dataTransfer.items;
+    if (files.length > 0) return dropfile(files);
     let type = e.dataTransfer.getData('component');
-    if(type == undefined || type.startsWith('c_')) return;
+    if (type == undefined || type.startsWith('c_')) return;
 
     main__contents.append(createComponent(type));
+}
+
+function dropfile(files) {
+    [...files].forEach((file) => {
+        let type = file.type.split('/')[0];
+        switch (type) {
+            case 'image':
+            case 'video':
+            case 'audio':
+                main__contents.append(createComponent(type, {file}));
+                break;
+            default:
+                console.warn('no support type :',type);
+                alert('지원하지 않는 파일형식 이거나 폴더 입니다');
+                break;
+        }
+    });
+
+    /*
+    if (ev.dataTransfer.items) {
+        // Use DataTransferItemList interface to access the file(s)
+        [...items].forEach((item, i) => {
+            // If dropped items aren't files, reject them
+            if (item.kind === "file") {
+                const file = item.getAsFile();
+                console.log(`… file[${i}].name = ${file.name}`);
+            }
+        });
+    }*/
 }
 function dragover(e) {
     e.preventDefault();
@@ -14,31 +44,32 @@ function dragstart(e) {
     e.dataTransfer.setData("component", e.target.getAttribute('type'));
 }
 
-function createComponent(type){
+function createComponent(type, option = {}) {
 
-    let id = 'c_'+Math.floor(Math.random() * 1000000).toString(16);
+    let id = 'c_' + Math.floor(Math.random() * 1000000).toString(16);
+    option.id = id;
 
     let spec = COMPONENT_SPEC[type];
 
     let component = document.createElement('div');
-    component.setAttribute('id',id);
-    component.setAttribute('draggable',true);
-    component.classList.add('component',type);
+    component.setAttribute('id', id);
+    component.setAttribute('draggable', true);
+    component.classList.add('component', type);
 
     let title = document.createElement('p');
     title.innerHTML = spec.title;
     component.append(title);
 
     let component__remove_btn = document.createElement('button');
-    component__remove_btn.classList.add('component__remove_btn','mdi','mdi-trash-can');
+    component__remove_btn.classList.add('component__remove_btn', 'mdi', 'mdi-trash-can');
     component.append(component__remove_btn);
-    
-    component.append(spec.option());
-    component.append(spec.input());
+
+    component.append(spec.option(option));
+    component.append(spec.input(option));
 
     component.ondragstart = (e) => {
         e.dataTransfer.setData("component", id);
-        e.dataTransfer.setDragImage(component,e.offsetX, e.offsetY);
+        e.dataTransfer.setDragImage(component, e.offsetX, e.offsetY);
     }
 
     component.ondrop = (e) => {
@@ -46,17 +77,19 @@ function createComponent(type){
         e.stopPropagation();
         let _id = e.dataTransfer.getData('component');
         let target;
-        if(_id == undefined) return;
-        if(_id.startsWith('c_')){
+        let files = e.dataTransfer.files || e.dataTransfer.items;
+        if (files.length > 0) return dropfile(files);
+        if (_id == undefined) return;
+        if (_id.startsWith('c_')) {
             target = document.getElementById(_id);
-        }else {
+        } else {
             target = createComponent(_id);
         }
-        if(component.getBoundingClientRect().height / 2 < e.offsetY) component.after(target);
+        if (component.getBoundingClientRect().height / 2 < e.offsetY) component.after(target);
         else component.before(target);
     }
 
-    component__remove_btn.onclick = function(){
+    component__remove_btn.onclick = function () {
         component.remove();
     }
 
@@ -66,10 +99,10 @@ function createComponent(type){
 const COMPONENT_SPEC = {
     textbox: {
         title: '텍스트 박스',
-        option: function(){
+        option: function () {
             return document.createDocumentFragment();
         },
-        input: function(){
+        input: function () {
             let element = document.createElement('div');
             element.setAttribute('contenteditable', "plaintext-only");
             element.setAttribute('placeholder', "여기에 텍스트를 입력하세요");
@@ -78,67 +111,82 @@ const COMPONENT_SPEC = {
     },
     image: {
         title: '이미지',
-        option: function(){
+        option: function () {
             return document.createDocumentFragment();
         },
-        input: function(){
+        input: function ({file}) {
+            if(file){
+                let el = new Image();
+                el.src = URL.createObjectURL(file);
+                return el;
+            }
             return document.createDocumentFragment();
         }
     },
     audio: {
         title: '음악',
-        option: function(){
+        option: function () {
             return document.createDocumentFragment();
         },
-        input: function(){
+        input: function ({file}) {
+            if(file){
+                let el = document.createElement('audio');
+                el.src = URL.createObjectURL(file);
+                return el;
+            }
             return document.createDocumentFragment();
         }
     },
     video: {
         title: '영상',
-        option: function(){
+        option: function () {
             return document.createDocumentFragment();
         },
-        input: function(){
+        input: function ({file}) {
+            if(file){
+                let el = document.createElement('video');
+                el.src = URL.createObjectURL(file);
+                return el;
+            }
             return document.createDocumentFragment();
         }
     },
     table: {
         title: '도표',
-        option: function(){
+        option: function () {
             return document.createDocumentFragment();
         },
-        input: function(){
+        input: function () {
             return document.createDocumentFragment();
         }
     },
     title: {
         title: '소제목',
-        option: function(){
+        option: function () {
             return document.createDocumentFragment();
         },
-        input: function(){
+        input: function () {
             return document.createDocumentFragment();
         }
     },
     seperator: {
         title: '구분선',
-        option: function(){
+        option: function () {
             return document.createDocumentFragment();
         },
-        input: function(){
+        input: function () {
             return document.createDocumentFragment();
         }
     },
 }
 
-window.addEventListener('load',function(){
+window.addEventListener('load', function () {
     main__contents = document.querySelector('.main__contents');
     main__contents.ondragover = dragover;
     main__contents.ondrop = drop;
 
     let component_list = document.querySelector('.component_list');
-    for(let specname in COMPONENT_SPEC){
+    for (let specname in COMPONENT_SPEC) {
         let spec = COMPONENT_SPEC[specname];
         let li = document.createElement('li');
         li.setAttribute('type', specname);
@@ -149,10 +197,10 @@ window.addEventListener('load',function(){
     }
 });
 
-function addSuggest(data, input){
+function addSuggest(data, input) {
     let li = document.createElement('li');
-    li.setAttribute('value',data.name);
-    li.onmousedown = ()=>{
+    li.setAttribute('value', data.name);
+    li.onmousedown = () => {
         li.parentNode.previousElementSibling.value = li.getAttribute('value');
     }
     input.querySelector('.input_suggest').append(li);
