8e9ab7c0c18c310e4a4de7b6a5c2b356d1d75547
asterisk <6507055@gmail.com>
2024-09-18 03:47:33 +0900

    버전 코드 변경

--- js/util.js
+++ js/util.js
@@ -565,7 +565,7 @@ let Listerners = {};
 
 let app = {}, app_aside = {}, app_article = {}, now = {};
 let devmode = false;
-let version = devmode ? new Date().getTime() : 'v2.3.0';
+let version = devmode ? new Date().getTime() : 'v2.3.1';
 const DOMAIN = 'nemuwiki.com';
 
 const OLD_ROOT_PATH = '/wiki';
