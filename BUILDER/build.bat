f8e779002306a39ac26803721a8b1644bf06fa6a
kim_hyeonwoo <6507055@gmail.com>
2024-09-20 17:25:20 +0900

    Update : 빌드스크립트 개선

--- BUILDER/build.bat
+++ BUILDER/build.bat
@@ -2,16 +2,28 @@
 chcp 65001 >nul
 setlocal
 
+set NODE_VERSION=v18.19.4
+set NODE_MSI=node-v%NODE_VERSION%-x64.msi
+set NODE_URL=https://nodejs.org/dist/%NODE_VERSION%/%NODE_MSI%
+
 :: Node.js 설치 여부 확인
 where node >nul 2>nul
 if %errorlevel% neq 0 (
     echo Node.js가 설치되어 있지 않습니다. 설치를 진행합니다.
     echo 다운로드 중...
-    powershell -Command "Invoke-WebRequest -Uri 'https://nodejs.org/dist/v18.19.4/node-v18.19.4-x64.msi' -OutFile 'nodejs.msi'"
+    powershell -Command "Invoke-WebRequest -Uri '%NODE_URL%' -OutFile '%NODE_MSI%'"
+    if %errorlevel% neq 0 (
+        echo Node.js 다운로드 실패
+        exit /b 1
+    )
     echo 설치 중...
-    msiexec /i nodejs.msi /quiet
+    msiexec /i %NODE_MSI% /quiet
+    if %errorlevel% neq 0 (
+        echo Node.js 설치 실패
+        exit /b 1
+    )
     echo Node.js 설치 완료
-    del nodejs.msi
+    del %NODE_MSI%
 ) else (
     echo Node.js가 설치되어 있습니다.
 )
@@ -23,14 +35,36 @@ npm list -g --depth=0 2>&1 | findstr /i "chokidar" >nul
 if %ERRORLEVEL% neq 0 (
     echo chokidar가 설치되어 있지 않습니다. chokidar를 설치 중입니다...
     npm install -g chokidar
+    if %errorlevel% neq 0 (
+        echo chokidar 설치 실패
+        exit /b 1
+    )
 ) else (
     echo chokidar가 이미 설치되어 있습니다.
+)
+
+echo terser 설치 여부 확인 중...
+
+:: terser 설치 여부 확인
+npm list -g --depth=0 2>&1 | findstr /i "terser" >nul
+if %ERRORLEVEL% neq 0 (
+    echo terser가 설치되어 있지 않습니다. terser를 설치 중입니다...
+    npm install -g terser
+    if %errorlevel% neq 0 (
+        echo terser 설치 실패
+        exit /b 1
+    )
+) else (
+    echo terser가 이미 설치되어 있습니다.
 )
 
 :: buildScript.js 스크립트 실행
 echo 병합 스크립트 실행 중...
 node buildScript.js
+if %errorlevel% neq 0 (
+    echo 스크립트 실행 중 오류 발생
+    exit /b 1
+)
 
 pause
-
 endlocal
--- BUILDER/buildScript.js
+++ BUILDER/buildScript.js
@@ -1,9 +1,9 @@
 const chokidar = require('chokidar');
 const { mergeAndSaveFiles, DuplicateFile } = require('./buildUtil');
 
-global.buildMode = 'product';
+global.buildMode = 'product'; // 또는 'development'
 
-//프로젝트 정보
+// 프로젝트 정보
 global.projectName = 'nemuwiki.com';
 global.projectNameKR = '네무위키';
 global.version = '2.3.1';
@@ -13,27 +13,27 @@ global.description = 'personal wiki project for NEMU';
 
 global.intro_js = function () {
     return `/**
-* Project: ${projectName}
-* Version: ${version} | ${global.buildMode}
-* Author: ${authorName}
-* Date: ${timestamp}
-* Description: ${description}
+* Project: ${global.projectName}
+* Version: ${global.version} | ${global.buildMode}
+* Author: ${global.authorName}
+* Date: ${global.timestamp}
+* Description: ${global.description}
 */`;
 };
 
 global.intro_html = function () {
-    return `<meta property="og:title" content="${projectNameKR}">
+    return `<meta property="og:title" content="${global.projectNameKR}">
     <meta property="og:locale" content="ko_KR">
-    <meta name="description" content="${description}">
-    <meta property="og:description" content="${description}">
-    <meta property="og:url" content="https://${projectName}">
-    <meta property="og:site_name" content="${projectNameKR}">
+    <meta name="description" content="${global.description}">
+    <meta property="og:description" content="${global.description}">
+    <meta property="og:url" content="https://${global.projectName}">
+    <meta property="og:site_name" content="${global.projectNameKR}">
     <meta property="og:type" content="website">
     <meta name="twitter:card" content="summary">
-    <meta property="twitter:title" content="${projectNameKR}">
-    <link rel="canonical" href="https://${projectName}">
+    <meta property="twitter:title" content="${global.projectNameKR}">
+    <link rel="canonical" href="https://${global.projectName}">
     <link rel="icon" href="/resource/favicon.png">`;
-}
+};
 
 // 병합할 파일이 있는 기본 디렉토리 경로
 const pageDestDir = '../js/page';
@@ -49,33 +49,57 @@ const templateDestPaths = [
     '../signup.html',
     '../profile.html',
     '../setting.html'
-]
+];
 
 // 디렉토리 변경 감지 및 병합 작업 실행
 const init = function () {
     console.log("########################################################");
-    console.log(`#####    Watching for changes in ${pageFromDir}    #####`);
+    console.log(`#####    Watching for changes in ${pageFromDir} and ${baseFromDir}    #####`);
     console.log("########################################################");
 
-    chokidar.watch([baseFromDir, pageFromDir, templateFromPath], { persistent: true }).on('all', (event, path) => {
-        switch (event) {
-            case 'unlinkDir':
-                return;
-        }
-        path = path.replaceAll('\\', '/');
-        if (path == templateFromPath) {
-            console.info(`[ WATCHER ]( ${event} ) - ${path}`);
-            DuplicateFile(templateFromPath, templateDestPaths);
-        } else if(path.startsWith(pageFromDir)){
-            let target = path.replace(pageFromDir, '').split('/')[1];
-            console.info(`[ WATCHER ]( ${event} ) - ${target || '_ROOT_'}`);
-            if (target && !target.endsWith('.js')) mergeAndSaveFiles(pageFromDir + '/' + target, false, pageDestDir);
-        } else {
-            let target = path.replace(baseFromDir, '').split('/')[1];
-            console.info(`[ WATCHER ]( ${event} ) - ${target || '_ROOT_'}`);
-            if (target && !target.endsWith('.js')) mergeAndSaveFiles(baseFromDir + '/' + target, false, baseDestDir);
+    const watcher = chokidar.watch([baseFromDir, pageFromDir, templateFromPath], { persistent: true });
+
+    watcher.on('all', (event, path) => {
+        try {
+            handleFileChange(event, path);
+        } catch (error) {
+            console.error(`[ ERROR ] - ${error.message}`);
         }
     });
 };
+
+const handleFileChange = (event, path) => {
+    path = path.replaceAll('\\', '/');
+    switch (event) {
+        case 'unlinkDir':
+            console.info(`[ WATCHER ]( ${event} ) - Directory deleted: ${path}`);
+            return;
+
+        case 'add':
+        case 'change':
+            if (path === templateFromPath) {
+                console.info(`[ WATCHER ]( ${event} ) - Template file changed: ${path}`);
+                DuplicateFile(templateFromPath, templateDestPaths);
+            } else if (path.startsWith(pageFromDir)) {
+                handleMergedFileChange(path, pageFromDir, pageDestDir);
+            } else {
+                handleMergedFileChange(path, baseFromDir, baseDestDir);
+            }
+            break;
+
+        default:
+            console.info(`[ WATCHER ]( ${event} ) - ${path}`);
+            break;
+    }
+};
+
+const handleMergedFileChange = (path, from, dest) => {
+    const target = path.replace(from, '').split('/')[1];
+    if (target && !target.endsWith('.js')) {
+        console.info(`[ WATCHER ]( Base ) - Merging files in: ${target || '_ROOT_'}`);
+        mergeAndSaveFiles(from + '/' + target, false, dest);
+    }
+
+}
 
 init();
